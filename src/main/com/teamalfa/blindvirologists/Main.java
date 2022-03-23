16fef472e6c67b11745e7c5e327d7b17f955ecf1
Lídia Tarcza <100163235+diatrcz@users.noreply.github.com>
2022-03-23 20:08:18 +0100

    hello

--- src/main/com/teamalfa/blindvirologists/Main.java
+++ src/main/com/teamalfa/blindvirologists/Main.java
@@ -1,10 +1,11 @@
 package main.com.teamalfa.blindvirologists;
 
 public class Main {
+
     public static void main(String args[]) {
         for(int i = 0 ; i < 40 ; i++) {
             System.out.println("Hello world!");
-            System.out.println("Hello Andris");
+            System.out.println("Hello meow");
         }
     }
 }

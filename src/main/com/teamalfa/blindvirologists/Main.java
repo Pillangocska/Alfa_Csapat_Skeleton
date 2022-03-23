ed525754445692052c0a501140f26b25380208d5
András Jankó <andras.janko@edu.bme.hu>
2022-03-23 19:42:03 +0100

    hali

--- src/main/com/teamalfa/blindvirologists/Main.java
+++ src/main/com/teamalfa/blindvirologists/Main.java
@@ -1,8 +1,9 @@
 package main.com.teamalfa.blindvirologists;
 
 public class Main {
-
     public static void main(String args[]) {
-        System.out.println("Hello world!");
+        for(int i = 0 ; i < 40 ; i++) {
+            System.out.println("Hello world!");
+        }
     }
 }

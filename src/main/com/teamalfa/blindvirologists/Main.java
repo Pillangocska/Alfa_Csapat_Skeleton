3fac669c591e9dee1ffad4a2cd0411ba600e4f62
krisztianszenasi <szenasi999@gmail.com>
2022-03-23 19:52:09 +0100

    Szia Andris.

--- src/main/com/teamalfa/blindvirologists/Main.java
+++ src/main/com/teamalfa/blindvirologists/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String args[]) {
         for(int i = 0 ; i < 40 ; i++) {
             System.out.println("Hello world!");
+            System.out.println("Hello Andris");
         }
     }
 }
